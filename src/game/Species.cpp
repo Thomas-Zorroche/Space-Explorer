6acc379baee881fbc5e5eae355eaa8e715dbc390
Thomas-Zorroche <thomas.zorroche@gmail.com>
2021-04-03 20:00:22 +0200

    [game] add Species class

---
+++ src/game/Species.cpp
@@ -0,0 +1,16 @@
+﻿#include "Species.hpp"
+
+Species::Species(
+	const std::string& name, 
+	int techLevel, 
+	const PlanetSettings& planetSettings)
+	: _name(name), _techLevel(techLevel), _bestPlanetSettings(planetSettings)
+{
+
+}
+
+const Species::PlanetSettings& bestPlanetSettings() const 
+{ 
+	return _bestPlanetSettings; 
+}
+
---
+++ src/game/Species.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "PlanetSettings.hpp"
+#include <string>
+
+class Species
+{
+public:
+	Species(const std::string& name, int techLevel, const PlanetSettings& planetSettings);
+
+	const PlanetSettings& bestPlanetSettings() const;
+
+private:
+	std::string _name;
+	int _techLevel;
+	PlanetSettings _bestPlanetSettings;
+};
