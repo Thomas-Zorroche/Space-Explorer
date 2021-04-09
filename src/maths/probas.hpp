2aac3b4e8ddc045e9254e7722e2109292e3901f4
Felix <felix.olart56@gmail.com>
2021-04-09 15:39:15 +0200

    [probas] oops Bernouilli function

--- src/maths/probas.hpp
+++ src/maths/probas.hpp
@@ -13,6 +13,8 @@ namespace probas
 	int discreteUniformDistribution(int a, int b);
 	// Coefficient binomial
     int nCr(int n, int r);
+    // Proba Binomiale
+    float binomialProbabilty(int n, int k, float p);
 
     // Tests des probas
     void TestProbas();
